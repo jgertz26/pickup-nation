1890cf653e200fce7244ce0e1cbfdd685f62e446
Jeremy <jgertz26@gmail.com>
2015-10-01 22:25:42 -0400

    added username, avatar, carrierwave

---
+++ README.md
@@ -0,0 +1,5 @@
+#PickupNation
+
+This app will let users schedule meetups for pickup basketball
+
+It's in the works folks
---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+class AvatarUploader < CarrierWave::Uploader::Base
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+end
