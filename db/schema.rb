1306930286f7c94f55c8d7dd0a320cc951d2fbbe
Jeremy <jgertz26@gmail.com>
2015-10-07 15:33:46 -0400

    admin added

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20151007190603) do
     t.string   "avatar"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
-    t.boolean  "admin",                  default: false
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
