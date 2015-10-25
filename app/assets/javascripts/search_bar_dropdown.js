4a140f369535b5eaf4826973468c08fc3e335647
Jeremy <jgertz26@gmail.com>
2015-10-24 20:08:40 -0400

    discover feature fixed

--- app/assets/javascripts/search_bar_dropdown.js
+++ app/assets/javascripts/search_bar_dropdown.js
@@ -1,4 +1,26 @@
 $("#discover-nav-button").click(function( event ) {
   event.preventDefault()
-  $('#dropdown-search').slideToggle( "slow" );
+  if ($(".search-container").length > 1 ) {
+
+    var backgroundColor = $(".search-container:last").css( "background-color" );
+    if (backgroundColor == "rgb(100, 100, 100)") {
+      $(".search-container:last").css({
+        "background-color": "rgba(0, 0, 0, 0)",
+        "box-shadow": "none"
+      });
+    }
+    else {
+      $('html, body').animate({
+        scrollTop: $(".search-container:last").offset().top
+      }, 400);
+      $(".search-container:last").css({
+        "background-color": "rgb(100, 100, 100)",
+        "box-shadow": "0px 0px 10px #2e8bad"
+      });
+    }
+  }
+
+  else {
+    $('#dropdown-search').slideToggle( "slow" );
+  }
 });
